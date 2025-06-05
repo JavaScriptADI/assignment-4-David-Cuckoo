eccacc7134dad0f2fd3dfe5d8b9e462be46be40e
David-Cuckoo <dgugulashvili1@gmail.com>
2025-06-05 10:39:58 +0400

    task 1 finished

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,12 +2,12 @@ let number = 0
 for (i=0; i<100; i++){
     number ++
 
-    if (number % 3 === 0){
-        console.log ("Fizz")}
-    if (number % 5 === 0){
-        console.log ("Buzz")}
     if (number % 3 === 0 && number % 5 === 0){
         console.log ("FizzBuzz")}
+    else if (number % 3 === 0){
+        console.log ("Fizz")}
+    else if (number % 5 === 0){
+        console.log ("Buzz")}
     else {console.log (number)}
 }
 
