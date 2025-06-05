8e6ba9bb12b490cc5c4af466c55389056beb6f7f
David-Cuckoo <dgugulashvili1@gmail.com>
2025-06-05 10:34:01 +0400

    task 1 smthn wrong again

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,6 +4,10 @@ for (i=0; i<100; i++){
 
     if (number % 3 === 0){
         console.log ("Fizz")}
+    if (number % 5 === 0){
+        console.log ("Buzz")}
+    if (number % 3 === 0 && number % 5 === 0){
+        console.log ("FizzBuzz")}
     else {console.log (number)}
 }
 
