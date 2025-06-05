7e2a313c94df07d1524282786cd56eb22346f56e
David-Cuckoo <dgugulashvili1@gmail.com>
2025-06-05 11:08:01 +0400

    task 2 still not working

--- random100.js
+++ random100.js
@@ -1,7 +1,5 @@
-let number = 0
 for (i = 0; i<100; i++){
-    let randomNumber = Math.floor(Math.random() * 100) + 1;
-    console.log ("randomNumber");
-}
+    let randomNumber = Math.floor(Math.random() * 100);}
+    
+    console.log (randomNumber);
 
-console.log (randomNumber)
